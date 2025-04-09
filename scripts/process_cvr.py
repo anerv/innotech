481aecd9ecc1e6dab269224186995ba7930a4740
anerv <vieroe@hotmail.com>
2025-04-09 09:32:42 +0200

    cvr process v1 done

--- scripts/process_cvr.py
+++ scripts/process_cvr.py
@@ -46,33 +46,94 @@ replace_dict = {
 
 for find_value, replace_value in replace_dict.items():
 
-    with open(new_cvr_path, "r") as f:
+    with open(new_cvr_path, "r", encoding="utf") as f:
         my_csv_text = f.read()
 
     new_csv_str = re.sub(find_value, replace_value, my_csv_text)
 
-    with open(new_cvr_path, "w") as f:
+    with open(new_cvr_path, "w", encoding="utf") as f:
         f.write(new_csv_str)
-
 
 cvr_data = pd.read_csv(new_cvr_path, sep=";", encoding="latin1")
 
 cvr_data_subset = cvr_data[cvr_data["hb_kode"].isin(cvr_codes.values())]
 assert len(cvr_data_subset) > 0, "No matching CVR codes found."
-# %%
 
-addresses = gpd.read_file("../data/cvr/adresser_utm.gpkg")
-addresses_access = gpd.read_file("../data/cvr/adgangs_adresser_utm.gpkg")
-
-# %%
-addresses.to_parquet("../data/cvr/adresser_utm.parquet", index=False)
-addresses_access.to_parquet("../data/cvr/adgangs_adresser_utm.parquet", index=False)
 # %%
 addresses = gpd.read_parquet("../data/cvr/adresser_utm.parquet")
 addresses_access = gpd.read_parquet("../data/cvr/adgangs_adresser_utm.parquet")
 
-# TODO: Match to addresses - check which ones?
+keep_cols_addresses = [
+    "id",
+    "status",
+    "vejnavn",
+    "husnr",
+    "etage",
+    "dør",
+    "postnr",
+    "kommunekode",
+    "adgangsadresse_status",
+    "adgangsadresseid",
+    "ddkn_km10",
+    "geometry",
+]
 
-# TODO: count and print how many of each type are matched/unmatched
+keep_cols_addresses_access = [
+    "id",
+    "status",
+    "vejnavn",
+    "husnr",
+    "postnr",
+    "kommunekode",
+    "ddkn_km10",
+    "geometry",
+]
+
+addresses = addresses[keep_cols_addresses]
+addresses_access = addresses_access[keep_cols_addresses_access]
+
+addresses = addresses.rename(
+    columns={
+        "status": "adr_status",
+    }
+)
+
+addresses_access = addresses_access.rename(
+    columns={
+        "status": "adr_status",
+    }
+)
+# %%
+cvr_address = addresses.merge(
+    cvr_data_subset,
+    left_on="id",
+    right_on="Adr_id",
+    how="right",
+)
+
+cvr_address["destination_type"] = cvr_address["hb_kode"].map(
+    {v: k for k, v in cvr_codes.items()}
+)
+
+print(
+    f"{len(cvr_address[cvr_address.Adr_id.notnull()])} CVR locations matched to addresses."
+)
+print(
+    f"{len(cvr_address[cvr_address.Adr_id.isnull()])} CVR locations not matched to addresses."
+)
+
+print("Unmatched CVR locations in each category:")
+cvr_address[cvr_address.Adr_id.isnull()]["destination_type"].value_counts()
+
+# %%
+# Export
+
+cvr_address.to_file(
+    "../results/cvr-destinations-all.gpkg",
+)
+
+cvr_address[cvr_address.Adr_id.notnull()].to_file(
+    "../results/cvr-destinations-w-address.gpkg",
+)
 
 # %%
