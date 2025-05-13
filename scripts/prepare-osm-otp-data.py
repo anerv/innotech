6b3b66eba504d434a2bf42d04f6a84444d246e20
anerv <vieroe@hotmail.com>
2025-05-13 14:18:33 +0200

    osm extract done

--- scripts/prepare-osm-otp-data.py
+++ scripts/prepare-osm-otp-data.py
@@ -6,6 +6,8 @@ import os
 from pathlib import Path
 import yaml
 import geopandas as gpd
+from shapely.ops import transform
+import subprocess
 
 with open(r"../config-data-prep.yml", encoding="utf-8") as file:
     parsed_yaml_file = yaml.load(file, Loader=yaml.FullLoader)
@@ -15,29 +17,37 @@ with open(r"../config-data-prep.yml", encoding="utf-8") as file:
 
 # %%
 
+datafolder = Path("/Users/anerv/repositories/innotech/data")
+input_pbf = datafolder / "input/osm/denmark-latest.osm.pbf"
+output_pbf = datafolder / "processed/osm/osm_study_area.pbf"
+clipfile = datafolder / f"processed/adm_boundaries/study_area.geojson"
+
+
+# %%
+# Function to remove Z-coordinate
+def remove_z(geometry):
+    if geometry.has_z:
+        return transform(lambda x, y, z=None: (x, y), geometry)
+    return geometry
+
+
 # make geojson of the study area
 study_area = gpd.read_file(study_area_fp)
 
-study_area
+study_area["geometry"] = study_area["geometry"].apply(remove_z)
 
+study_area.to_crs(epsg=4326, inplace=True)
+
+study_area.to_file(clipfile, driver="GeoJSON")
+
+# %%
 # check to see if osmium is available
-# TODO !osmium --version
-
-
-# %%
-
-
-datafolder = Path("/Users/anerv/repositories/innotech/data")
-input_pbf = datafolder / "input/osm/denmark-latest.osm.pbf"
-output_pbf = datafolder / "processed/osm/region_sj.pbf"
-clipfile = datafolder / "processed/adm_boundaries/regionsj.geojson"
-
-# %%
-os.system(f"osmium extract -p {clipfile} -o {output_pbf} {input_pbf}")
-
-# %%
-import subprocess
-
 batcmd = "osmium --version"
 result = subprocess.check_output(batcmd, shell=True)
+print(result.decode("utf-8"))
+
+# %%
+# extract osm data for the study area
+os.system(f"osmium extract -p {clipfile} -o {output_pbf} {input_pbf}")
+
 # %%
