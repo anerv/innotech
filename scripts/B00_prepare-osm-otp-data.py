2cbe3caea5e87709083bef10014ab85915284913
anerv <vieroe@hotmail.com>
2025-05-15 15:49:44 +0200

    working on graph generation

--- scripts/B00_prepare-osm-otp-data.py
+++ scripts/B00_prepare-osm-otp-data.py
@@ -17,6 +17,8 @@ with open(r"../config-data-prep.yml", encoding="utf-8") as file:
 
 # %%
 
+### PREPATE OSM DATA FOR OTP
+
 datafolder = Path("/Users/anerv/repositories/innotech/data")
 input_pbf = datafolder / "input/osm/denmark-latest.osm.pbf"
 output_pbf = datafolder / "otp/osm_study_area.pbf"
@@ -43,5 +45,37 @@ print(result.decode("utf-8"))
 # %%
 # extract osm data for the study area
 os.system(f"osmium extract -p {clipfile} -o {output_pbf} {input_pbf}")
+
+# %%
+#### BUILD OTP GRAPH
+
+# TODO: test
+
+otp_folder = Path("/Users/anerv/repositories/innotech/otp")
+osm_pbf = otp_folder / "osm_study_area.pbf"
+netex_file = otp_folder / "netex"
+
+otp_exists = any(
+    f.name == "otp-shaded-2.7.0.jar" for f in otp_folder.iterdir() if f.is_file()
+)
+
+netex_zip_exists = any(
+    f.name.endswith("NeTEx.zip") for f in otp_folder.iterdir() if f.is_file()
+)
+osm_pbf_exists = any(
+    f.name == "osm_study_area.pbf" for f in otp_folder.iterdir() if f.is_file()
+)
+config_exists = any(
+    f.name == "build-config.json" for f in otp_folder.iterdir() if f.is_file()
+)
+
+os.chdir("c:\\Users\\anerv\\repositories\\innotech\\otp\\")
+
+if netex_zip_exists and osm_pbf_exists and config_exists and otp_exists:
+    print("All required files are in place, building OTP graph...")
+    os.system("java -Xmx2G -jar otp-shaded-2.7.0.jar --build --save .")
+
+else:
+    print("Missing input data: Netex zip file or osm pbf file not found in otp folder!")
 
 # %%
