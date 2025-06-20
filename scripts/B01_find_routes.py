19b4c21fc194dcfd21a55b0a86f5c107f11f1abe
anerv <vieroe@hotmail.com>
2025-06-20 16:21:30 +0200

    working on second run

--- scripts/B01_find_routes.py
+++ scripts/B01_find_routes.py
@@ -32,24 +32,86 @@ otp_db_fp = (
 
 walk_speed = config_model["walk_speed"]  # Load the walk speed in m/s
 search_window = config_model["search_window"]  # Load the search window in seconds
+
+search_window_first_run = 7200  # the search window for the first run in seconds
 # %%
 
 # DubckDB connection
 con = duckdb.connect()
 
-# Open a persistent DuckDB database file Data is stored temporay in Dockdb and then exported to parquet
+# Open a persistent DuckDB database file Data is stored temporay in Duckdb and then exported to parquet
 otp_con = duckdb.connect(otp_db_fp)
 
 # %%
 services = config_model["services"]
 
-for service in services:
+for service in services[1:]:
     for i in range(1, int(service["n_neighbors"]) + 1):
         dataset = f"{service['service_type']}_{i}"
         # Process each dataset
         print(
-            f"Processing {dataset} with sample size {sample_size} and chunk size {chunk_size}"
+            f"Processing {dataset} with sample size {sample_size}, chunk size {chunk_size} and search window {search_window_first_run} seconds"
         )
+        process_adresses(
+            dataset,
+            sample_size,
+            service["arival_time"],
+            date,
+            walk_speed,
+            search_window_first_run,
+            url,
+            data_path,
+            otp_con,
+            con,
+            chunk_size,
+            parallelism,
+        )
+        # Export to parquet
+        tabelname = dataset.replace("-", "_")
+        otp_con.execute(
+            f"""
+            COPY (SELECT * FROM {tabelname}) TO '{results_path}/{dataset}_otp.parquet' (FORMAT 'parquet')
+        """
+        )
+
+# %%
+# TODO: implement a second run with a larger search window
+
+
+import pandas as pd
+import os
+
+services = config_model["services"]
+
+for service in services[0:1]:
+    for i in range(1, int(service["n_neighbors"]) + 1):
+        dataset = f"{service['service_type']}_{i}"
+
+        # Load the results
+        print(f"Loading results for {dataset}...")
+
+        results = pd.read_parquet(f"{results_path}/{dataset}_otp.parquet")
+
+        input_data = pd.read_parquet(f"{data_path}/{dataset}.parquet")
+
+        input_data_no_solution = input_data[
+            input_data["source_address_id"].isin(
+                results[results.duration.isna()]["source_id"]
+            )
+        ]
+
+        input_data_no_solution.to_parquet(
+            f"{data_path}/{dataset}_second_run.parquet", index=False
+        )
+
+        print(
+            f"Processing {dataset} with sample size {sample_size}, chunk size {chunk_size} and search window {search_window} seconds"
+        )
+        print(f"New search window: {search_window} seconds")
+
+        dataset = f"{dataset}_second_run"  # Update dataset name for second run
+
+        # set this as input for the next run
         process_adresses(
             dataset,
             sample_size,
@@ -72,4 +134,34 @@ for service in services:
         """
         )
 
+        results_first_run = results[results.duration.notna()]
+
+        results_second_run = pd.read_parquet(f"{results_path}/{dataset}_otp.parquet")
+
+        # Combine the results
+        combined_results = pd.concat(
+            [results_first_run, results_second_run], ignore_index=True
+        )
+
+        assert (
+            combined_results.shape[0]
+            == results_first_run.shape[0] + results_second_run.shape[0]
+        ), "Combined results do not match the expected number of rows"
+
+        # reset dataset name for export
+        dataset = f"{service['service_type']}_{i}"
+
+        # Export the combined results
+        combined_results.to_parquet(
+            f"{results_path}/{dataset}_combined_otp.parquet", index=False
+        )  # TODO: once results are cleared, just export the combined results to the original file
+
+        # remove exported files from second run
+        # os.remove(f"{results_path}/{dataset}_second_run_otp.parquet")
+        # os.remove(f"{data_path}/{dataset}_second_run.parquet")
+
+
+# %%
+# %%
+test = pd.read_parquet("../results/data/doctor_1_otp.parquet")
 # %%
