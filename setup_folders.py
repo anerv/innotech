044fe456fa0d73532037d1782910d23cbef7ced2
anerv <vieroe@hotmail.com>
2025-06-25 15:25:58 +0200

    removed result plots

--- setup_folders.py
+++ setup_folders.py
@@ -36,7 +36,7 @@ for f in processed_subfolders:
 
         print("Successfully created folder: " + os.path.join(processed_base, f))
 
-result_subfolders = ["data", "maps", "plots"]
+result_subfolders = ["data", "maps"]
 
 for f in result_subfolders:
     if not os.path.exists(os.path.join("results", f)):
